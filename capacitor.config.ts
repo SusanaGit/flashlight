f7b40529b22d7b742dfeb630c88ea6eaf4a157b8
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-07-07 11:40:41 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'flashlight',
+  webDir: 'www'
+};
+
+export default config;

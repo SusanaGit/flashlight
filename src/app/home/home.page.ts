83aa8023df10eb9b099e43a72f169a85fae51403
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-07-07 13:25:25 +0200

    05. create ion-grid into home.page.html content and create a boolean active into home.page.ts

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import {IonHeader, IonToolbar, IonTitle, IonContent, IonGrid, IonRow, IonCol, IonText} from '@ionic/angular/standalone';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonGrid, IonRow, IonCol, IonText],
 })
 export class HomePage {
-  constructor() {}
+
+  public active: boolean;
+
+  constructor() {
+    this.active = false;
+  }
 }

61c741bcc07dbe9cbc07436ee96c2f1cac974fc7
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-07-10 23:51:24 +0200

    14. call the element loader before the bootstrapModule/bootstrapApplication call

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
+import { defineCustomElements } from '@ionic/pwa-elements/loader';
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
+
+// Call the element loader before the bootstrapModule/bootstrapApplication call
+defineCustomElements(window);
 
 bootstrapApplication(AppComponent, {
   providers: [
